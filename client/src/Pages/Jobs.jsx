77661255e06e8b19404786cffcb9f79fe6666112
Sanket Jaiswal <103429178+SanketJ29@users.noreply.github.com>
2024-02-20 14:23:03 +0530

    Update Jobs.jsx

--- client/src/Pages/Jobs.jsx
+++ client/src/Pages/Jobs.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from '../Components/Navbar';
 import Input from '../Components/Input';
-// import service2 from "../assets/5ea96f8d38467_Emergency.jpg"
 import manager from '../assets/manager.jpg'
 import wind from '../assets/wind.jpg'
 import solar from '../assets/solar.jpg'
@@ -9,6 +8,19 @@ import ecologist from '../assets/ecologist.jpg'
 import { Link } from 'react-router-dom';
 
 const Jobs = () => {
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+  const [stakingBalance, setStakingBalance] = useState(0);
+  const [rewardBalance, setRewardBalance] = useState(50);
+  const handleStake = () => {
+    setStakingBalance(stakingBalance + 10);
+  };
+
+  const handleUnstake = () => {
+    setStakingBalance(stakingBalance - 5);
+  };
+  const handleButtonClick = () => {
+    setIsButtonClicked(true);
+  };
     return (
         <div>
             <Navbar/>
@@ -33,8 +45,9 @@ const Jobs = () => {
                 This role boost 30% annual growing green job rate, & job postings have surged by 27.6%. They contribute significantly with an impactful 82% positive effect in pollution prevention.
                 </p>
                 <div className='flex ml-20 items-center'>
-                <button class="inline-flex items-center bg-purple-800 border-0 py-1 px-3 focus:outline-none hover:bg-blue-500 rounded text-base md:mt-0">
-            <Link to="/" className="text-white font-mono text-xl">Stake Tokens</Link></button>
+                <button class="inline-flex items-center bg-purple-800 border-0 py-1 px-3 focus:outline-none hover:bg-blue-500 rounded text-base md:mt-0 " onClick={handleButtonClick}>
+            <div className="text-white font-mono text-xl">Stake Tokens</div>
+            </button>
           </div>
               </div>
             </div>
@@ -54,8 +67,8 @@ const Jobs = () => {
                 This role boost 24% annual growing green job rate, & job postings have surged by 20%. They contribute significantly with an impactful 74% positive effect in ecosystem management.
                 </p>
                 <div className='flex ml-20 items-center'>
-                <button class="inline-flex items-center bg-purple-800 border-0 py-1 px-3 focus:outline-none hover:bg-blue-500 rounded text-base md:mt-0">
-            <Link to="/" className="text-white font-mono text-xl">Stake Tokens</Link></button>
+                <button class="inline-flex items-center bg-purple-800 border-0 py-1 px-3 focus:outline-none hover:bg-blue-500 rounded text-base md:mt-0"onClick={handleButtonClick}>
+            <div className="text-white font-mono text-xl">Stake Tokens</div></button>
           </div>
               </div>
             </div>
@@ -74,7 +87,7 @@ const Jobs = () => {
                 This role boost 23% annual growing green job rate, & job postings have surged by 19%. They contribute with an impactful 62.5% positive effect in renewable eneregy generation.
                 </p>
                 <div className='flex ml-20 items-center'>
-                <button class="inline-flex items-center bg-purple-800 border-0 py-1 px-3 focus:outline-none hover:bg-blue-500 rounded text-base md:mt-0">
+                <button class="inline-flex items-center bg-purple-800 border-0 py-1 px-3 focus:outline-none hover:bg-blue-500 rounded text-base md:mt-0" onClick={handleButtonClick}>
             <Link to="/" className="text-white font-mono text-xl">Stake Tokens</Link></button>
           </div>
               </div>
@@ -94,8 +107,8 @@ const Jobs = () => {
                 This role boost 22% annual growing green job rate, & job postings have surged by 17%. They contribute significantly with an impactful 60% positive effect in ecosystem management.
                 </p>
                 <div className='flex ml-20 items-center'>
-                <button class="inline-flex items-center bg-purple-800 border-0 py-1 px-3 focus:outline-none hover:bg-blue-500 rounded text-base md:mt-0">
-            <Link to="/" className="text-white font-mono text-xl">Stake Tokens</Link></button>
+                <button class="inline-flex items-center bg-purple-800 border-0 py-1 px-3 focus:outline-none hover:bg-blue-500 rounded text-base md:mt-0" onClick={handleButtonClick}>
+            <p className="text-white font-mono text-xl">Stake Tokens</p></button>
           </div>
               </div>
             </div>
@@ -103,24 +116,32 @@ const Jobs = () => {
         </div>    
         </section>
         
+        {isButtonClicked && (
         <section class="text-gray-400 body-font bg-gray-900">
         <div class="container px-5 mx-auto">
-            <h1 class="text-4xl font-medium title-font mb-10 text-cyan-400 text-center font-mono">AVAIL SERVICES</h1>
+            <h1 class="text-4xl font-medium title-font mb-10 text-purple-400 text-center font-mono">AVAIL SERVICES</h1>
           <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
-          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism mx-auto mb-20">
+          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center bg-purple-400 bg-opacity-20 rounded-lg mx-auto mb-20">
+            <p class="leading-relaxed text-base text-lg text-gray-300 font-mono ">Staking Balance: {stakingBalance} mDAI</p>
+            <p class='leading-relaxed text-base text-lg text-gray-300 font-mono mb-4'>Reward Balance: {rewardBalance} DApp</p>
+            
             <Input placeholder="Address To" name="addressTo" type="text"  />
             <Input placeholder="Amount (ETH)" name="amount" type="number" />
-            <Input placeholder="Which service you need" name="keyword" type="text" />
-            <Input placeholder="Enter Message" name="message" type="text"  />
             <div className="mt-2">
-              <button className="bg-cyan-900 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded focus:outline-none focus:shadow-outline active:bg-cyan-800 font-mono text-2xl">
-                SEND
+              <button className="bg-purple-800 hover:bg-blue-700 text-white font-mono py-2 px-3 rounded focus:outline-none focus:shadow-outline active:bg-purple-400 font-mono text-xl">
+                STAKE
+              </button>
+            </div>
+            <div className="mt-2">
+              <button className="bg-white hover:bg-blue-200 text-purple-800 font-mono py-2 px-3 rounded focus:outline-none focus:shadow-outline active:bg-purple-400 font-mono text-xl">
+                UNSTAKE
               </button>
             </div>
           </div>
           </div>
         </div>
         </section>
+        )}
     </div>
     );
 };
